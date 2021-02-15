4fb78dac6f98575ba333b87a2c3f0e5684f0c806
Hermann Marx <marxhermann@web.de>
2021-02-15 14:06:40 +0100

    added database connection and API endpoints

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,35 +2,10 @@ const pool = require("../dbconfig");
 
 module.exports = {
   getAll: async (req, res) => {
-    try {
-      const data = await pool.query("SELECT * FROM products");
-      res.json({
-        code: 200,
-        operation: "success",
-        description: "Fetched all products",
-        data: data.rows,
-      });
-    } catch (e) {
-      console.error(Error(e));
-      res.status(500).send("Something happened, Hermann");
-    }
+    res.send("Hello All Products");
   },
   getById: async (req, res) => {
     const { id } = req.params;
-    try {
-      const data = await pool.query(
-        "SELECT * FROM products WHERE product_id=$1",
-        [id]
-      );
-      res.json({
-        code: 200,
-        operation: "success",
-        description: `Fetched product Nr.${id}`,
-        data: data.rows,
-      });
-    } catch (e) {
-      console.error(Error(e));
-      res.status(500).send("Something happened, Hermann");
-    }
+    res.send("Hello Product by Id");
   },
 };
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 
 const users = require("./routes/users");
 const products = require("./routes/products");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use("/", users);
 app.use("/", products);
